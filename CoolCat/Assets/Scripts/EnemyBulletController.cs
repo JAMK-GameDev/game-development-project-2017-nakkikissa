a44da21f418a420fbf08704a9e40fc9e7a61a468
Pewpo <samulir@windowslive.com>
2017-04-04 21:46:16 +0300

    Repaired enemy prefab and added shooting scripts

---
+++ CoolCat/Assets/Scripts/EnemyBulletController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletController : MonoBehaviour {
+
+    float speed; 
+    Vector2 _direction; // The direction of the bullet
+    bool isReady; //to know when the bullet direction is set
+
+    //set default values in awake function
+    void Awake() {
+        speed = 5f;
+        isReady = false;
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public void SetDirection(Vector2 direction) {
+        _direction = direction.normalized; // to get an unit vector
+        isReady = true; 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isReady == true) {
+            //get bullet current position
+            Vector2 position = transform.position;
+            //new position
+            position += _direction * speed * Time.deltaTime;
+            transform.position = position;
+
+            //bottom-left & top-right 
+            Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+            Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+            //if bullet goes outside the screen -> destroy
+            if ((transform.position.x < min.x) || (transform.position.x > max.x) ||
+              (transform.position.y < min.y) || (transform.position.y > max.y)) {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ CoolCat/Assets/Scripts/EnemyGun.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGun : MonoBehaviour {
+    public GameObject EnemyBullet; // enemybullet prefab
+	// Use this for initialization
+	void Start () {
+        Invoke("FireEnemyBullet", 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    void FireEnemyBullet() {
+        GameObject mouse = GameObject.Find("hero");
+        if(mouse != null) {
+            //instantiate an enemy bullet
+            GameObject bullet = (GameObject)Instantiate(EnemyBullet);
+            //set the bullet's initial position
+            bullet.transform.position = transform.position;
+            //compute the bullet's direction towards the player
+            Vector2 direction = mouse.transform.position - bullet.transform.position;
+            //set the bullet's direction
+            bullet.GetComponent<EnemyBulletController>().SetDirection(direction);
+        }
+    }
+}
