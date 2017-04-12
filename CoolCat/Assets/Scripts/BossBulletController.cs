55b845779eede47e3dbd2fc173206d41b3dcf9b4
Pewpo <samulir@windowslive.com>
2017-04-12 14:16:00 +0300

    Added Boss enemy

---
+++ CoolCat/Assets/Scripts/BossBulletController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBulletController : MonoBehaviour
+{
+    float speed;
+    Vector2 _direction; // The direction of the bullet
+    bool isReady = false; //to know when the bullet direction is set
+
+    //set default values in awake function
+    void Awake()
+    {
+        speed = 7f;
+        isReady = false;
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    public void SetDirection(Vector2 direction)
+    {
+        _direction = direction.normalized; // to get an unit vector
+        isReady = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isReady == true)
+        {
+            //get bullet current position
+            Vector2 position = transform.position;
+            //new position
+            position += _direction * speed * Time.deltaTime;
+            transform.position = position;
+
+            //bottom-left & top-right 
+            Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+            Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+            //if bullet goes outside the screen -> destroy      
+            if ((transform.position.x < min.x) || (transform.position.x > max.x) ||
+              (transform.position.y < min.y) || (transform.position.y > max.y))
+            {
+                Destroy(gameObject);
+
+            }
+        }
+
+    }
+}
---
+++ CoolCat/Assets/Scripts/BossGun.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossGun : MonoBehaviour {
+    public GameObject BossBullet; // enemybullet prefab
+    public float fireRate = 0.3F;
+    private float nextFire = 0.0F;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Time.time > nextFire)
+        {
+            Invoke("FireBossBullet", 1f);
+            nextFire = Time.time + fireRate;
+        }
+    }
+
+
+    void FireBossBullet()
+    {
+        GameObject mouse = GameObject.Find("hero");
+        if (mouse != null)
+        {
+            //instantiate an enemy bullet
+            GameObject bullet = (GameObject)Instantiate(BossBullet);
+            //set the bullet's initial position
+            bullet.transform.position = transform.position;
+            //compute the bullet's direction towards the player
+            Vector2 direction = mouse.transform.position - bullet.transform.position;
+            //set the bullet's direction
+            bullet.GetComponent<BossBulletController>().SetDirection(direction);
+        }
+    }
+}
