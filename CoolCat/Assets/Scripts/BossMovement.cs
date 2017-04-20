8ccdf03512411a5b5d4a7e6fc2c8ef59abc1a5b4
Pewpo <H9577@Labranet.jamk.fi>
2017-04-20 11:18:59 +0300

    Adding movement script

--- CoolCat/Assets/Scripts/BossMovement.cs
+++ CoolCat/Assets/Scripts/BossMovement.cs
@@ -2,27 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossMovement : MonoBehaviour {
+public class BossMovement : MonoBehaviour
+{
     public float speed;
     Rigidbody2D myBody;
     Transform myTrans;
     int facingLeft = 1;
     GameObject hero;
     GameObject boss;
+    float newposition;
 
-    void Start () {
+
+    void Start()
+    {
         myTrans = this.transform;
         myBody = this.GetComponent<Rigidbody2D>();
         hero = GameObject.FindGameObjectWithTag("Player");
         boss = GameObject.FindGameObjectWithTag("Boss");
+        newposition = boss.transform.position.y - 2;
     }
-	
 
-	void FixedUpdate () {
-        
-        Vector2 myVel = myBody.velocity;
-        myVel.y = myTrans.right.y * speed;
-        myBody.velocity = myVel;
+
+    void FixedUpdate()
+    {
+        if (boss.transform.position.y < newposition)
+        {
+                Vector2 myVel = myBody.velocity;
+                myVel.y = myTrans.position.y * speed;
+                myBody.velocity = myVel;
+                if (boss.transform.position.y > newposition)
+                {
+                    newposition = boss.transform.position.y - 2;
+                  }           
+        }
+        else if (boss.transform.position.y > newposition)
+        {         
+                Vector2 myVel = myBody.velocity;
+                myVel.y = -myTrans.position.y * speed;
+                myBody.velocity = myVel;
+                if (boss.transform.position.y < newposition)
+                {
+                    newposition = boss.transform.position.y + 2;
+                }          
+        }
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
 
@@ -33,30 +56,30 @@ public class BossMovement : MonoBehaviour {
         }
     }
     void BossFlip()
-    {       
-            if (hero.transform.position.x < boss.transform.position.x && facingLeft == 1)
-            {
-               // speed = 0;
-            }
-            else if (hero.transform.position.x < boss.transform.position.x && facingLeft == 0)
-            {
-                Vector3 currRot = myTrans.eulerAngles;
-                currRot.y += 180;
-                myTrans.eulerAngles = currRot;
-                speed = 0;
-                facingLeft = 1;
-            }
-            else if (hero.transform.position.x > boss.transform.position.x && facingLeft == 0)
-            {
-               // speed = 0;
-            }
-            else if (hero.transform.position.x > boss.transform.position.x && facingLeft == 1)
-            {
-                Vector3 currRot = myTrans.eulerAngles;
-                currRot.y += 180;
-                myTrans.eulerAngles = currRot;
-                facingLeft = 0;
-                speed = 0;
-            }   
+    {
+        if (hero.transform.position.x < boss.transform.position.x && facingLeft == 1)
+        {
+            // speed = 0;
+        }
+        else if (hero.transform.position.x < boss.transform.position.x && facingLeft == 0)
+        {
+            Vector3 currRot = myTrans.eulerAngles;
+            currRot.y += 180;
+            myTrans.eulerAngles = currRot;
+            speed = 0;
+            facingLeft = 1;
+        }
+        else if (hero.transform.position.x > boss.transform.position.x && facingLeft == 0)
+        {
+            // speed = 0;
+        }
+        else if (hero.transform.position.x > boss.transform.position.x && facingLeft == 1)
+        {
+            Vector3 currRot = myTrans.eulerAngles;
+            currRot.y += 180;
+            myTrans.eulerAngles = currRot;
+            facingLeft = 0;
+            speed = 0;
+        }
     }
 }
